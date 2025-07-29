0d15c3575ff4292ddb48bde38f4c56254384a9c2
maurotechdev <maurodiogo098@gmail.com>
2025-07-29 12:26:24 +0100

    Actualização

--- Eventos e Funções/js/locadora.js
+++ Eventos e Funções/js/locadora.js
@@ -2,11 +2,11 @@ function locadora()
 {
     var Titulo = document.getElementById("titulo").value;
     var Duracao = document.getElementById("duracao").value;
-    
+
     // Exibe os valores na div com id "respoder..."
-    document.getElementById("responderTitulo").innerHTML = "O título do filme é: " + Titulo;
+    document.getElementById("responderTitulo").textContent = "O título do filme é: " + Titulo;
     
-    document.getElementById("responderDuracao").innerHTML = "A duração do filme" + Titulo + " é de: " + Duracao/60 +" e " + Duracao%60 + " minutos";
+    document.getElementById("responderDuracao").textContent = "A duração do filme" + Titulo + " é de: " + Math.floor(Duracao/60) +" e " + Duracao%60 + " minutos";
 }
 
 // registra para o botão "calcular" um ouvinte para o evento click,
